8918645deddf392e5900453fbf294ed9286d7deb
Mark_Koenig <markkoenig03@gmail.com>
2018-07-11 18:45:46 -0400

    re add normalize.css

--- js/myScript.js
+++ js/myScript.js
@@ -1,11 +1,11 @@
 //main.js//
 /*dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
 
     var dropdowns = document.getElementsByClassName("dropdown-content");
